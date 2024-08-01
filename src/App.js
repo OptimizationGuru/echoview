db725eb4fc0cb9baff356a3e498a27457cfa8326
Shivam Tiwari <shivam.tiwari@yulu.bike>
2024-08-01 13:26:11 +0530

    basic structure created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Body from "./components/Body";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="m-0 p-0">
+      <Header/>
+      <Body/>
+      {/* 
+        
+        Header
+        Body
+        Sidebar
+        MainContainer
+        ButtonList
+        VideoContainer
+        VideoCard
+        WatchComponenet
+        VideoPlayer
+        Comments
+        Sugesstions
+        Sugesstions
+       */}
     </div>
   );
 }
